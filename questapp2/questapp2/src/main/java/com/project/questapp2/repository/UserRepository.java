41556ecc65fdd04476d07db18bc7f33c38b586e9
EsraDuyguUguz <154420003+EsraDuyguUguz@users.noreply.github.com>
2024-07-19 01:13:28 +0300

    Delete questapp2/questapp2 directory

--- questapp2/questapp2/src/main/java/com/project/questapp2/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.questapp2.repository;
-
-import com.project.questapp2.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long>{
-
-    User findByUserName(String username);
-}
